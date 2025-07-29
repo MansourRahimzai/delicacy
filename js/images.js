fb5b44920fd7679b74cacdb424eaf31887892b02
Mansour <mansourrahimi008@gmail.com>
2025-07-29 14:07:41 +0100

    images load problem solved

--- js/images.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const images = document.querySelectorAll("img");
-  images.forEach((img) => {
-    img.setAttribute("loading", "lazy");
-  });
-});
